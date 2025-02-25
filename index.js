cb5a9ec96edafb2c122a8c0a4c334c6a75bd4a40
inwoo7 <68881292+inwoo7@users.noreply.github.com>
2025-02-25 12:21:31 -0800

    Add index.js wrapper for Render compatibility

---
+++ index.js
@@ -0,0 +1,5 @@
+// This file is a simple wrapper to run server.js
+// It's created to handle Render's default behavior of running index.js
+
+console.log('Starting server via index.js wrapper...');
+require('./server.js');
